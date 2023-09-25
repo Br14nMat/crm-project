70bf0d7a7808e0eb718743da49ab835f43173339
Andres Arango <arango850@gmail.com>
2023-09-25 10:51:55 -0500

    feat(HU12): Changes in Nit and primary key

--- CRM/core/models.py
+++ CRM/core/models.py
@@ -2,12 +2,12 @@ from django.db import models
 
 # Create your models here.
 class InvestigationProyect(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, primary_key=True, unique=True)
     description = models.TextField()
     objetivos = models.TextField()
     start_date= models.DateField()
     finish_date= models.DateField()
-    nit= models.CharField(max_length=15)
+    nit= models.IntegerField()
 
     def __str__(self):
         return self.name
