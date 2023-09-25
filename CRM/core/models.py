6f973c0dd127111b016afa3b88585c6f8f1ed52c
Andres Arango <arango850@gmail.com>
2023-09-25 07:53:48 -0500

    feat(HU12): Creation of the model for the investigation projects

---
+++ CRM/core/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+class InvestigationProyect(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    objetivos = models.TextField()
+    start_date= models.DateField()
+    finish_date= models.CharField(max_length=15)
+
+    def __str__(self):
+        return self.name
