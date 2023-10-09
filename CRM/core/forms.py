e17bfad465fd755f54ab8142918fb1036d50b82f
jpnino1018 <1005897746@u.icesi.edu.co>
2023-10-09 11:02:39 -0500

    style: Added style

--- CRM/core/forms.py
+++ CRM/core/forms.py
@@ -46,3 +46,12 @@ class FollowupForm(ModelForm):
     class Meta:
         model = Followup
         exclude = ("event_id",)
+        widgets = {
+            'name': widgets.TextInput(attrs = {'class': 'form-control'}),
+
+            'description': widgets.Textarea(attrs = {
+                'class': 'form-control',
+                'rows': '2'
+                }),
+
+        }
